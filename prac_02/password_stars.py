567da2c46aeb90c5f76c6d70ba3132af4338b58b
Blake <blake.knight@my.jcu.edu.au>
2022-09-23 11:56:28 +1000

    Refactor password checker program to use function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,21 @@
-password = input("Password:")
-password_length = len(password)
-while password_length < 10:
+def main():
+    password_length = get_password()
+    while password_length < 10:
+        print_stars(password_length)
+        print("invalid password length, must be at least 10 letters")
+        password_length = get_password()
+    print_stars(password_length)
+    print("valid password length")
+
+
+def print_stars(password_length):
     print('*' * password_length)
-    print("invalid password length, must be at least 10 letters")
+
+
+def get_password():
     password = input("Password:")
     password_length = len(password)
-print('*' * password_length)
-print("valid password length")
+    return password_length
+
+
+main()
