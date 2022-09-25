a29aed92fd8ff2ca59be229dde212e2dc85e1a3d
Blake <blake.knight@my.jcu.edu.au>
2022-09-25 21:40:01 +1000

    Refactor password checker program to use function

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,34 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score >= 100 or score < 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Passable")
+
+def get_score():
+    global score
+    score = float(input("Enter score: "))
+
+
+def main():
+    if score >= 100 or score < 0:
+        print("Invalid score")
+    elif score < 50:
+        print("Bad")
+    elif score >= 90:
+        print("Excellent")
+    else:
+        print("Passable")
+
+
+def get_random_number():
+    global score
+    from random import randint
+    score = randint(0, 100)
+    print("random score value", score)
+
+
+get_score()
+
+main()
+
+get_random_number()
+
+main()
