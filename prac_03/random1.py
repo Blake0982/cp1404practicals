e2b17d4d789a4f5a764e89b22528a1731bc92d92
Blake <blake.knight@my.jcu.edu.au>
2022-09-30 11:09:02 +1000

    Committed and pushed files to git

---
+++ prac_03/random1.py
@@ -0,0 +1,13 @@
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# line 1, the smallest number would be 5 and the largest 20
+# line 2, the smallest number would be 3 and the largest 9
+# line 3, the smallest number would be 2.500000000000001 and the largest 5.499999999999999
+
+print(random.randint(1, 100))
+
