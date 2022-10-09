911e5f09f1488803df65c9e5e6a720f36a178f86
Blake <blake.knight@my.jcu.edu.au>
2022-10-09 10:28:40 +1000

    Committed and pushed lists_warmup.py file to git

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,12 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0]; 3
+# numbers[-1]; 2
+# numbers[3]; 1
+# numbers[:-1]; [3, 1, 4, 1, 5, 9]
+# numbers[3:4]; [1]
+# 5 in numbers; true
+# 7 in numbers; false
+# number + [6, 5, 3]; [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+numbers = ["ten", 1, 4, 1, 5, 9, 1]
+print(numbers[2:7])
+print(9 in numbers)
