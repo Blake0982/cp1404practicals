6d5581fd348f606820f51617e977e3c8b4b8b142
Blake <blake.knight@my.jcu.edu.au>
2022-10-09 14:03:51 +1000

    committed and pushed final list_exercises.py file to git

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,12 +1,27 @@
-numbers = []
+def number_part():
+    numbers = []
+    for i in range(0, 5, 1):
+        numbers = numbers + [int(input("Number: "))]
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest nuber is {max(numbers)}")
+    average = sum(numbers) / len(numbers)
+    print(f"The average of the numbers is {average}")
 
-for i in range(0, 5, 1):
-    numbers = numbers + [int(input("Number: "))]
+
+number_part()
 
 
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest nuber is {max(numbers)}")
-average = sum(numbers) / len(numbers)
-print(f"The average of the numbers is {average}")
+def username_part():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn',
+                 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    user_input = input("username:")
+    if user_input in usernames:
+        print("Access Granted")
+    else:
+        print("Access denied")
+
+
+username_part()
