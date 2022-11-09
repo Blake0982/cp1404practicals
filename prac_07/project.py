08ebefa4ff8af8867aec9364a2308609f590d9fd
Blake <blake.knight@my.jcu.edu.au>
2022-11-09 19:06:42 +1000

    commit completed projects files

--- prac_07/project.py
+++ prac_07/project.py
@@ -4,8 +4,7 @@ Project class
 
 
 class Project:
-
-    def __int__(self, name, start_date, priority, cost_estimate, completion_percentage):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
         self.start_data = start_date
         self.priority = priority
@@ -14,9 +13,8 @@ class Project:
 
     def __str__(self):
         """the string returned if the object is printed"""
-        return f"Project:{self.name}, Start Date:{self.start_data}, Priority{self.priority}," \
-               f"Cost Estimate:{self.cost_estimate}, {self.completion_percentage}% complete "
+        return f"{self.name}, Start:{self.start_data}, Priority {self.priority}," \
+               f"Estimate: ${self.cost_estimate}, Completion: {self.completion_percentage}% "
 
     def __lt__(self, other):
         return self.priority < other.priority
-
