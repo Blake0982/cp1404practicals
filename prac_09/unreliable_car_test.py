06802e66b0cdd7609566f50a73e1c3eebaff35f9
Blake <blake.knight@my.jcu.edu.au>
2022-11-23 17:13:34 +1000

    commit unreliable_car codes

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,14 @@
+"""
+conducts two test objects one on the high reliability and one on the low reliability
+"""
+from unreliable_car import UnreliableCar
+
+my_reliable_car = UnreliableCar(100, "Prius 1", 99)
+print(my_reliable_car)
+my_reliable_car.drive(40)
+print(my_reliable_car)
+
+my_unreliable_car = UnreliableCar(100, "Holden Cruz", 20)
+print(my_unreliable_car)
+my_unreliable_car.drive(40)
+print(my_unreliable_car)
