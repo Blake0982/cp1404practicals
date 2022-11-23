d5a0544e6802e516d76e71279b0d242ccc75d548
Blake <blake.knight@my.jcu.edu.au>
2022-11-23 16:40:58 +1000

    commit walkthrough example codes taxis

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+from taxi import Taxi
+
+my_taxi = Taxi(100, "Prius 1")
+print(my_taxi)
+my_taxi.drive(40)
+print(my_taxi)
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(f"{my_taxi}, Current fare: ${my_taxi.get_fare()}")
